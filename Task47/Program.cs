21a487f69d497b05d394753242de4855d669ca08
AnaSilvestrova <silvestrovaab@gmail.com>
2023-07-07 21:23:01 +0700

    random double array created

---
+++ Task47/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+double [,] CreateArray(int row, int col)
+{
+    Random rand = new Random();
+    double[,] array = new double[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = rand.NextDouble();
+        }
+    }
+    return array;
+}
+
+void PrintArray(double [,] array){ 
+    int row = array.GetLength(0); 
+    int col = array.GetLength(1); 
+    for (int i = 0; i < row; i++) 
+    { 
+        for (int j = 0; j < col; j++) 
+        { 
+            Console.Write($"{array[i,j]} ");
+        } 
+        Console.WriteLine(); 
+    } 
+} 
+
+Console.Clear(); 
+double [,] array = CreateArray(3,4); 
+PrintArray(array); 
+Console.WriteLine(); 
